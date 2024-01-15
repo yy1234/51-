50faebf3b12f86e09202990668f74b29389b21f8
yy1234 <270089758@qq.com>
2024-01-15 17:55:42 +0800

    测试提交

--- static_display/main.c
+++ static_display/main.c
@@ -1,10 +1,60 @@
 #include <stdio.h>
 #include <REGX52.H>
+void niXie(unsigned char location, number)
+{
+    switch (location)
+    {
+    case 1:
+        P2_4 = 1;
+        P2_3 = 1;
+        P2_1 = 1;
+        break;
+    case 2:
+        P2_4 = 1;
+        P2_3 = 1;
+        P2_1 = 1;
+        break;
+    case 3:
+        P2_4 = 1;
+        P2_3 = 1;
+        P2_1 = 1;
+        break;
+    case 4:
+        P2_4 = 1;
+        P2_3 = 1;
+        P2_1 = 1;
+        break;
+    case 5:
+        P2_4 = 1;
+        P2_3 = 1;
+        P2_1 = 1;
+        break;
+    case 6:
+        P2_4 = 1;
+        P2_3 = 1;
+        P2_1 = 1;
+        break;
+    case 7:
+        P2_4 = 1;
+        P2_3 = 1;
+        P2_1 = 1;
+        break;
+        case 8:
+        P2_4 = 1;
+        P2_3 = 1;
+        P2_1 = 1;
+        break;    
+    default:
+        break;
+    }
+}
 int main()
 {
     P2_4 = 1;
     P2_3 = 0;
     P2_1 = 1;
     P0 = 0x7D;
+    while (1)
+        ;
     return 0;
 }
